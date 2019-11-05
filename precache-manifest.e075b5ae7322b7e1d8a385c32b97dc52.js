e14186785ea12b85b8d7597cde1c3d460330e5dc
Ilya Faybisovich <ilya.faybisovich@gmail.com>
2019-11-05 14:57:22 +0100

    Updates

---
+++ precache-manifest.e075b5ae7322b7e1d8a385c32b97dc52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a240e9d81725767b43c23a393b96c88",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3965286386931415071d",
+    "url": "/monsters-rolodex/static/css/main.4bceb749.chunk.css"
+  },
+  {
+    "revision": "7b2f39d7df2c69c0af67",
+    "url": "/monsters-rolodex/static/js/2.8fbd90c1.chunk.js"
+  },
+  {
+    "revision": "3965286386931415071d",
+    "url": "/monsters-rolodex/static/js/main.e595c069.chunk.js"
+  },
+  {
+    "revision": "d0427e354a0eb3d56b1b",
+    "url": "/monsters-rolodex/static/js/runtime-main.10deb336.js"
+  }
+]);
